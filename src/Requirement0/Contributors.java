1d2b2d0e2c7747cef8d83ecdf09b2004cc492f5e
Al Sibai, Nathan (k1630623) <felixampharos@gmail.com>
2017-03-07 14:10:46 +0000

    Re-initialised repo.
    Eclipse compatibility should no longer be an issue.

---
+++ src/Requirement0/Contributors.java
@@ -0,0 +1,15 @@
+
+package Requirement0;
+
+
+public class Contributors {
+
+    public static void main(String[] args) {
+        //creating main method to force change then commit
+        
+        System.out.println("Nathan Al Sibai");
+        System.out.println("Sophia Kalanovska");
+        System.out.println("Arthur Dantcikian");
+        System.out.println("Kelvin van Vuuren");
+    }
+}
