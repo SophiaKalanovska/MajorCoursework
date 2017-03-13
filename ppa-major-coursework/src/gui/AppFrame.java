5fb0cd3f33f24b7bff529e4b7e7b5938923c3ad9
Al Sibai, Nathan (k1630623) <felixampharos@gmail.com>
2017-03-13 22:48:14 +0000

    Updated annotations in AppFrame.

--- ppa-major-coursework/src/gui/AppFrame.java
+++ ppa-major-coursework/src/gui/AppFrame.java
@@ -4,21 +4,28 @@ import java.awt.Dimension;
 
 import javax.swing.JFrame;
 
-public class AFrame extends JFrame {
+public class AppFrame extends JFrame {
 
-	public AFrame() {
+	public AppFrame() {
 		super();
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//need to set some sort of layout manager
 		setPreferredSize(new Dimension(800, 600));
 		pack();
 		setLocationRelativeTo(null);
+		initWidgets();
 	}
 	
 	public void display() {
 		setVisible(true);
 	}
 	
+	public void initWidgets() {
+		//add assignments (and adding) of JPanels here
+	}
+	
 	public static void main (String[] args) {
-		AFrame myFrame = new AFrame();
+		AppFrame myFrame = new AppFrame();
 		myFrame.display();
 	}
 	
